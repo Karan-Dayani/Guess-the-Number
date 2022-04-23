9efc3756aa321f3b3352ac6b1da33dd459adb1f9
EvilZeus69 <93029074+EvilZeus69@users.noreply.github.com>
2022-04-23 16:25:17 +0530

    lamo ded

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ inputBox.addEventListener("keydown", function (e) {
 var randomNum = Math.floor(Math.random() * 101);
 var btn = document.querySelector(".btn");
 var result = document.querySelector(".result")
+var range = document.querySelector(".heading2")
 
 btn.onclick = function () {
     if (Number(inputBox.value) > randomNum) {
-        result.innerHTML = "Too High";
+        range.innerHTML = "Too High";
     } else if (Number(inputBox.value) === randomNum) {
         result.innerHTML = "You won";
     } else if (Number(inputBox.value) < randomNum) {
-        result.innerHTML = "Too Low";
+        range.innerHTML = "Too Low";
     }
 }
