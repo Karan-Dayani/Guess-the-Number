851a9cd8cd5445e57aa65d55f025614b974ff7ac
EvilZeus69 <93029074+EvilZeus69@users.noreply.github.com>
2022-04-23 16:21:55 +0530

    2

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 var inputBox = document.getElementById("inputBox");
 
+
 var invalidChars = [
-  "-",
-  "+",
-  "e",
+    "-",
+    "+",
+    "e",
 ];
 
-inputBox.addEventListener("keydown", function(e) {
-  if (invalidChars.includes(e.key)) {
-    e.preventDefault();
-  }
+inputBox.addEventListener("keydown", function (e) {
+    if (invalidChars.includes(e.key)) {
+        e.preventDefault();
+    }
 });
 
 
-var randomNum = Math.floor(Math.random()*101);
+var randomNum = Math.floor(Math.random() * 101);
 var btn = document.querySelector(".btn");
+var result = document.querySelector(".result")
 
-btn.onclick = function() {
-  if (Number(inputBox.value) > randomNum) {
-    console.log("Too High");
-  } else if (Number(inputBox.value) === randomNum) {
-    console.log("You won");
-  } else if (Number(inputBox.value) < randomNum) {
-    console.log("Too Low");
-  }
+btn.onclick = function () {
+    if (Number(inputBox.value) > randomNum) {
+        result.innerHTML = "Too High";
+    } else if (Number(inputBox.value) === randomNum) {
+        result.innerHTML = "You won";
+    } else if (Number(inputBox.value) < randomNum) {
+        result.innerHTML = "Too Low";
+    }
 }
