08ec0372ee65f499d7acf98f47272e19fb1f0d15
EvilZeus69 <93029074+EvilZeus69@users.noreply.github.com>
2022-04-23 15:59:56 +0530

    6

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var inputBox = document.getElementById("inputBox");
+var randomNum = Math.floor(Math.random()*101)
+var inValue = inputBox.innerHTML
+var btn = document.querySelector(".btn")
+
+
+btn.onclick(()=>{
+    console.log(inValue)
+})
+
 
 var invalidChars = [
   "-",
