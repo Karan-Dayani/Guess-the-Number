4225a5ded9454344106743b9bbd8c438362d11c2
EvilZeus69 <93029074+EvilZeus69@users.noreply.github.com>
2022-04-23 15:48:33 +0530

    css ded

---
+++ index.js
@@ -0,0 +1,13 @@
+var inputBox = document.getElementById("inputBox");
+
+var invalidChars = [
+  "-",
+  "+",
+  "e",
+];
+
+inputBox.addEventListener("keydown", function(e) {
+  if (invalidChars.includes(e.key)) {
+    e.preventDefault();
+  }
+});
